72dc0fdd4a192b90831d3742f1f5633df173666b
mvalentine65 <mvalentine65@gmail.com>
2021-06-15 01:20:52 -0400

    Adds partial Bob reply function.

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    //    unimplemented!("have Bob reply to the incoming message: {}", message)
+    // if all upper question:   'Calm down, I know what I'm doing!'
+    // if all upper statement:  'Whoa, chill out!'
+    // if regular question:     'Sure.'
+    // if regular statement:    'Whatever.'
+    // if all whitespace:       'Fine. Be that way!'
+    let input: String = message
+        .chars()
+        .filter(|char| !char.is_whitespace())
+        .collect::<String>();
 }

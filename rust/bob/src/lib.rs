41770f156cb5e6a42b4a7da529b2bf613b41b832
mvalentine65 <mvalentine65@gmail.com>
2021-06-16 21:53:55 -0400

    Implements solution for bob.
    It feels kind of ugly but it doesn't have any variables so that's
    pretty cool. Maybe I should take the plunge and just try functional
    programming. Does it matter if Haskell consumes my being?

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,12 +1,19 @@
 pub fn reply(message: &str) -> &str {
-    //    unimplemented!("have Bob reply to the incoming message: {}", message)
-    // if all upper question:   'Calm down, I know what I'm doing!'
-    // if all upper statement:  'Whoa, chill out!'
-    // if regular question:     'Sure.'
-    // if regular statement:    'Whatever.'
-    // if all whitespace:       'Fine. Be that way!'
-    let input: String = message
-        .chars()
-        .filter(|char| !char.is_whitespace())
-        .collect::<String>();
+    match message.trim() {
+        x if x.is_empty() => "Fine. Be that way!",
+        x if x.to_uppercase() == x
+            && !(x.ends_with('?'))
+            && x.matches(char::is_alphabetic).count() > 0 =>
+        {
+            "Whoa, chill out!"
+        }
+        x if x.to_uppercase() == x
+            && x.ends_with('?')
+            && x.matches(char::is_alphabetic).count() > 0 =>
+        {
+            "Calm down, I know what I'm doing!"
+        }
+        x if x.ends_with('?') => "Sure.",
+        _ => "Whatever.",
+    }
 }

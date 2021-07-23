4d5936f8c6713ad15658b7870e58b4ca41cc1ed8
mvalentine65 <mvalentine65@gmail.com>
2021-07-23 00:49:49 -0400

    Finishes clock solution

--- rust/Easy/gigasecond/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use chrono::{DateTime, Utc};
-
-// Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start
-        .checked_add_signed(chrono::Duration::seconds(1000000000))
-        .unwrap()
-}
--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,41 @@
-pub struct Clock;
+use std::fmt;
 
-impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+
+#[derive(Debug,PartialEq)]
+pub struct Clock {
+        hours: i32,
+        minutes: i32,
     }
 
+
+fn find_minutes(hours:i32, minutes: i32) -> i32 {
+    let new_minutes:i32 = (hours * 60 + minutes) % 1440;
+    if new_minutes > 0 { return new_minutes; }
+    else { return 1440 + new_minutes; }
+}
+
+
+impl fmt::Display for Clock {
+fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result  {
+       write!(f, "{:02}:{:02}",self.hours, self.minutes) 
+    }
+}
+
+impl Clock {
+    
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let total_minutes:i32 = find_minutes(hours, minutes);
+        Self {
+            hours: (total_minutes / 60) % 24,
+            minutes: total_minutes % 60,
+        }
+    }
+        
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+           let total_minutes:i32 = find_minutes(self.hours, self.minutes + minutes);
+           Self {
+               hours: (total_minutes / 60) % 24,
+               minutes: total_minutes % 60,
+           }
     }
 }
